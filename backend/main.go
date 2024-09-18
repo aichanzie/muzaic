40ec350829a166aeaac136eb007684520bb22f3e
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-09-18 10:54:54 -0400

    start backend

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,36 @@
-package main 
+package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"encoding/json"
+    "github.com/gorilla/mux"
+)
 
 
-func main() {
-	fmt.Println("Hello world")
+func greetingsHandler(w http.ResponseWriter,r *http.Request){
+    fmt.Fprintf(w,"Greeting from Go Server 👋")
+}
+
+// For the POST request 
+type Founder struct{
+    Name string `json:"name"`
+    Age uint32 `json:"age"`
+    Email string `json:"email"`
+    Company string `json:"company"`
+}
+
+var founders []Founder 
+
+founders = append(founders, Founder{Name:"Mehul",Age:23,Email:"random@random.com",Company: "BharatX"})
+
+func main(){
+    r := mux.NewRouter()
+
+    r.HandleFunc("/",greetingsHandler).Methods("GET")
+
+    fmt.Println("Hello from GoServer 👋")
+    fmt.Printf("Starting server at port 8000\n")
+    log.Fatal(http.ListenAndServe(":8000",r))
 }
